f01efa951a9287519a7ff566401a157ea9acd01a
Almat Kulbaev <akulbaev@ozon.ru>
2023-05-18 09:24:26 +0300

    add

--- src/pages/companies/CompaniesPage.tsx
+++ src/pages/companies/CompaniesPage.tsx
@@ -1,18 +1,62 @@
 import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
-import { Avatar, List, Space } from 'antd';
+import { Avatar, Input, List, Select, Space, Tag } from 'antd';
 
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
 
 import { useGetCompaniesListQuery } from 'api/routes/companiesApi';
 
 import './CompaniesPage.scss';
 
+import { COMPANIES_MOCK } from 'helpers/mocks/Companies.mock';
+
+import type { CustomTagProps } from 'rc-select/lib/BaseSelect';
+
 const CompaniesPage: React.FC = () => {
-    const { data } = useGetCompaniesListQuery();
+    const navigate = useNavigate();
+
+    const [selectedPositions, setSelectedPositions] = useState<number[]>([]);
+    const [filteredName, setFilteredName] = useState<string>('');
+
+    const tagRender = ({ label, value, closable, onClose }: CustomTagProps) => {
+        const onPreventMouseDown = (event: React.MouseEvent<HTMLSpanElement>) => {
+            event.preventDefault();
+            event.stopPropagation();
+            onClose();
+        };
+        return (
+            <Tag onMouseDown={onPreventMouseDown} onClose={onClose} className='lp-tag lp-tag_filter'>
+                {label}
+            </Tag>
+        );
+    };
+
+    const data = COMPANIES_MOCK.companies.filter((company)=>(company.name.toLocaleLowerCase().includes(filteredName)))
 
     return (
-        <>
-            <div>Найдено комапний: 10</div>
+        <div className='companies-page'>
+        {/* <div className='filter-wrapper'>
+                <label>Позиция</label>
+                <Select
+                    mode='multiple'
+                    allowClear
+                    tagRender={tagRender}
+                    onChange={(val) => {
+                        setSelectedPositions(val);
+                        handleFilter({ positions: val });
+                    }}
+                    options={positionsOptions}
+                    optionFilterProp='label'
+                />
+            </div> */}
+            <Input
+                value={filteredName}
+                onChange={(e) => setFilteredName(e.target.value.toLocaleLowerCase())}
+                placeholder='Поиск'
+                className='students-filters__search'
+            />
+            <div>Найдено комапний: {data.length}</div>
             <List
                 className='list'
                 itemLayout='vertical'
@@ -23,7 +67,7 @@ const CompaniesPage: React.FC = () => {
                 //     },
                 //     pageSize: 3,
                 // }}
-                dataSource={data?.companies}
+                dataSource={data}
                 // footer={
                 //     <div>
                 //         <b>ant design</b> footer part
@@ -31,13 +75,22 @@ const CompaniesPage: React.FC = () => {
                 // }
                 renderItem={(item) => (
                     <List.Item
-                        // key={item.title}
+                        className='list_item'
+                        key={item.name}
+                        onClick={
+                            ()=>{  navigate(`${item.id}`)}
+                        }
                         // actions={[
                         //     <IconText icon={StarOutlined} text='156' key='list-vertical-star-o' />,
                         //     <IconText icon={LikeOutlined} text='156' key='list-vertical-like-o' />,
                         //     <IconText icon={MessageOutlined} text='2' key='list-vertical-message' />,
                         // ]}
-                        extra={<div className='label'>"qwqw"</div>}
+                        extra={
+                        // <div className='label'>"qwqw"</div>
+                        <Tag className={`lp-tag lp-tag_yellow`}>
+                            <div>План {item.plan} | Взяли {item.taken}</div>
+                    </Tag>
+                    }
                     >
                         {/* <List.Item.Meta
                     avatar={<Avatar src={item.avatar} />}
@@ -48,7 +101,7 @@ const CompaniesPage: React.FC = () => {
                     </List.Item>
                 )}
             />
-        </>
+        </div>
     );
 };
 
