d9670f09cebd877b82afc2c3698ef5b2e361cc77
Almat Kulbaev <49912185+clmct@users.noreply.github.com>
2023-05-18 11:46:29 +0300

    262 - Страница списка компаний (#3)

--- src/pages/companies/CompaniesPage.tsx
+++ src/pages/companies/CompaniesPage.tsx
@@ -1,5 +1,15 @@
+import React from 'react';
+
+import CompaniesList from 'components/views/companies-list/CompaniesList';
+
+import './CompaniesPage.scss';
+
 const CompaniesPage: React.FC = () => {
-    return <div>Компании</div>;
+    return (
+        <div className='companies-page'>
+            <CompaniesList />
+        </div>
+    );
 };
 
 export default CompaniesPage;
