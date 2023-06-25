0b98200d6db43ca3ae5fa930bb45aaaa1156c1fa
Artykova Kamilla <kartykova@ntr.ai>
2023-06-25 20:14:54 +0700

    avatar updated

--- src/components/shared/AppAvatar.tsx
+++ src/components/shared/AppAvatar.tsx
@@ -1,15 +1,17 @@
 import { Avatar } from 'antd';
 import { AvatarSize } from 'antd/es/avatar/SizeContext';
 
-const AppAvatar: React.FC<{ src: string; size?: AvatarSize | number; style?: React.CSSProperties }> = ({
-    src,
+const AppAvatar: React.FC<{ src: string; alt?: string; size?: AvatarSize | number; style?: React.CSSProperties }> = ({
+    src = 'https://www.meme-arsenal.com/memes/ec12b2d95f12a919311035ddfd7c03e2.jpg',
+    alt = 'avatar',
     size = 'default',
     style,
 }) => (
     <Avatar
+        src={<img onError={(e) => (e.currentTarget.style.display = 'none')} src={src} alt='' />}
         size={size}
         style={{ backgroundColor: '#ccc', ...style }}
-        src={<img onError={(e) => (e.currentTarget.style.display = 'none')} src={src} alt='avatar' />}
+        alt={alt}
     />
 );
 
